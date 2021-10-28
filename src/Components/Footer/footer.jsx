32cc65ab6fda522e2b2d600f9b6cfddeb0a1f094
Akshara Purwar <aksharapurwar@gmail.com>
2021-10-28 17:16:30 +0530

    [Akshara Purwar] added footer component

---
+++ src/Components/Footer/footer.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import './footer.scss'
+
+export class footer extends Component {
+    render() {
+        return (
+            <>
+                <div className="footer-main">
+                    <p className="footer-title">Copyright &copy; 2020, Bookstore Private Limited. All Rights Reserved</p>
+                </div>
+
+
+
+
+            </>
+        )
+    }
+}
+
+export default footer
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,11 +1,13 @@
-import React, { Component } from 'react'
-import Header from '../../Components/header/header'
+import React, { Component } from 'react';
+import Header from '../../Components/header/header';
+import Footer from '../../Components/Footer/footer';
 
 export class home extends Component {
     render() {
         return (
             <div>
                 <Header/>
+                <Footer/>
                 
             </div>
         )
